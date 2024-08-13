0dc3408ac82c57e67076fd955b06a2197063f229
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-08-13 14:32:06 +0700

    favicon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,6 +20,8 @@
     <!-- Custom styles for this template-->
     <link href="<?= base_url(); ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
 
+    <link rel="icon" type="image/x-icon" href="<?= base_url('assets/img/logo/btn_favicon.png'); ?>">
+
 </head>
 
 <body id="page-top">
