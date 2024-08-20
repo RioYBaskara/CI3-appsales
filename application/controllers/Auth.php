dbfb6d855fe1d263ab4d5a09dd277678451c0bdf
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-08-20 11:25:47 +0700

    fitur login selesai, tinggal rbac

---
+++ application/controllers/Auth.php
@@ -0,0 +1,74 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * Buat ngilangin red line, karena intelphense
+ *  @property form_validation $form_validation 
+ *  @property load $load 
+ *  @property input $input 
+ *  @property db $db
+ *  @property session $session
+ *  @property Sales_model $Sales_model
+ *  @property Pks_model $Pks_model
+ *  @property Nasabah_model $Nasabah_model
+ *  @property Closing_model $Closing_model
+ *  @property Aktivitas_model $Aktivitas_model
+ *  @property Menu_model $Menu_model
+ *  @property Submenu_model $Submenu_model
+ *  @property Role_model $Role_model
+ *  @property Access_model $Access_model
+ *  @property Users_model $Users_model
+ */
+
+class Auth extends CI_Controller
+{
+    public function index()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+        if ($this->form_validation->run() == FALSE) {
+            $data['title'] = 'Halaman Login';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $this->_login();
+        }
+    }
+
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('users', ['email' => $email])->row_array();
+
+        // jika user ada
+        if ($user) {
+            // cek password
+            if (password_verify($password, $user['password'])) {
+                $data = [
+                    'email' => $user['email'],
+                    'role_id' => $user['role_id']
+                ];
+                $this->session->set_userdata($data);
+                redirect('users');
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Salah!</div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email tidak ditemukan!</div>');
+            redirect('auth');
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out!</div>');
+        redirect('auth');
+    }
+}
