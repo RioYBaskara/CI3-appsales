3618168992721a046b4c0fa9886fbc6f63b7b2c9
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-09-01 01:00:32 +0700

    controller pks crud selesai, sisa view

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -607,7 +607,6 @@ class Data extends CI_Controller
 
         $this->form_validation->set_rules('id_sales', 'Nama Sales', 'required');
         $this->form_validation->set_rules('id_nasabah', 'Nama Nasabah', 'required');
-        $this->form_validation->set_rules('tanggal', 'Tanggal', 'required');
         $this->form_validation->set_rules('no_pks', 'Nomor PKS', 'required');
         $this->form_validation->set_rules('tanggal_awal_pks', 'Tanggal Awal PKS', 'required');
         $this->form_validation->set_rules('tanggal_akhir_pks', 'Tanggal Akhir PKS', 'required');
@@ -670,59 +669,44 @@ class Data extends CI_Controller
 
     public function pksedit()
     {
-        $data['title'] = 'Edit Closing';
+        $data['title'] = 'Edit PKS';
         $data['user'] = $this->db->get_where('users', ['email' => $this->session->userdata('email')])->row_array();
 
         $role_id = $this->session->userdata("role_id");
         $id_sales = $data['user']['id_sales'];
 
-        $this->db->select('closing.*, nasabah.nama_nasabah, sales.nama_sales');
-        $this->db->from('closing');
-        $this->db->join('nasabah', 'nasabah.id_nasabah = closing.id_nasabah', 'left');
-        $this->db->join('sales', 'sales.id_sales = closing.id_sales', 'left');
+        $this->db->select('pks.*, nasabah.nama_nasabah, sales.nama_sales');
+        $this->db->from('pks');
+        $this->db->join('nasabah', 'nasabah.id_nasabah = pks.id_nasabah', 'left');
+        $this->db->join('sales', 'sales.id_sales = pks.id_sales', 'left');
 
         if ($role_id != 1) {
-            $this->db->where('closing.id_sales', $id_sales);
+            $this->db->where('pks.id_sales', $id_sales);
         }
 
-        $data['closing'] = $this->db->get()->row_array();
+        $data['pks'] = $this->db->get()->row_array();
         $data['sales'] = $this->db->get('sales')->result_array();
         $data['sales'] = $this->db->get('nasabah')->result_array();
 
         $this->form_validation->set_rules('id_sales', 'Nama Sales', 'required');
         $this->form_validation->set_rules('id_nasabah', 'Nama Nasabah', 'required');
-        $this->form_validation->set_rules('tanggal', 'Tanggal', 'required');
-        $this->form_validation->set_rules('nominal_closing', 'Nominal', 'required');
+        $this->form_validation->set_rules('no_pks', 'Nomor PKS', 'required');
+        $this->form_validation->set_rules('tanggal_awal_pks', 'Tanggal Awal PKS', 'required');
+        $this->form_validation->set_rules('tanggal_akhir_pks', 'Tanggal Akhir PKS', 'required');
 
         if ($this->form_validation->run() == FALSE) {
             $this->load->view('templates/header', $data);
             $this->load->view('templates/sidebar', $data);
             $this->load->view('templates/topbar', $data);
-            $this->load->view('data/closing', $data);
+            $this->load->view('data/pks', $data);
             $this->load->view('templates/footer');
         } else {
-            $tanggal = $this->input->post('tanggal');
-            $timestamp = strtotime($tanggal);
-            $hariInggris = date('l', $timestamp);
-
-            $namaHariIndonesia = [
-                'Sunday' => 'Minggu',
-                'Monday' => 'Senin',
-                'Tuesday' => 'Selasa',
-                'Wednesday' => 'Rabu',
-                'Thursday' => 'Kamis',
-                'Friday' => 'Jumat',
-                'Saturday' => 'Sabtu'
-            ];
-
-            $hari = $namaHariIndonesia[$hariInggris];
-
             $data_update = [
                 'id_sales' => $this->input->post('id_sales'),
                 'id_nasabah' => $this->input->post('id_nasabah'),
-                'tanggal' => $tanggal,
-                'hari' => $hari,
-                'nominal_closing' => $this->input->post('nominal_closing'),
+                'no_pks' => $this->input->post('no_pks'),
+                'tanggal_awal_pks' => $this->input->post('tanggal_awal_pks'),
+                'tanggal_akhir_pks' => $this->input->post('tanggal_akhir_pks'),
             ];
 
             $upload_image = $_FILES['image']['name'];
@@ -730,14 +714,14 @@ class Data extends CI_Controller
             if ($upload_image) {
                 $config['allowed_types'] = 'gif|jpg|png';
                 $config['max_size'] = '2048';
-                $config['upload_path'] = './assets/img/closing';
+                $config['upload_path'] = './assets/img/pks';
 
                 $this->load->library('upload', $config);
 
                 if ($this->upload->do_upload('image')) {
-                    $old_image = $data['closing']['upload_foto'];
+                    $old_image = $data['pks']['upload_foto'];
                     if ($old_image != 'default.jpg') {
-                        unlink(FCPATH . 'assets/img/closing/' . $old_image);
+                        unlink(FCPATH . 'assets/img/pks/' . $old_image);
                     }
 
                     $new_image = $this->upload->data('file_name');
@@ -747,14 +731,14 @@ class Data extends CI_Controller
                     $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '<button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button></div>');
-                    redirect('data/closing');
+                    redirect('data/pks');
                 }
             }
 
-            $this->db->where('id_closing', $this->input->post('id_closing'));
-            $this->db->update('closing', $data_update);
+            $this->db->where('id_pks', $this->input->post('id_pks'));
+            $this->db->update('pks', $data_update);
             $this->session->set_flashdata("flashswal", "Diubah");
-            redirect('data/closing');
+            redirect('data/pks');
         }
     }
 
@@ -763,7 +747,7 @@ class Data extends CI_Controller
     {
         $this->db->delete("pks", ["id_pks" => $id_pks]);
         $this->session->set_flashdata("flashswal", "Dihapus");
-        redirect('data/closing');
+        redirect('data/pks');
     }
 
 }
