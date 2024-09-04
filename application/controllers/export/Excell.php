5b495d89abdd96fdb0cb6415bea99ebd58c4441e
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-09-04 09:19:57 +0700

    testing export excell dummy, berhasil

---
+++ application/controllers/export/Excell.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+/**
+ * Buat ngilangin red line, karena intelphense
+ *  @property form_validation $form_validation 
+ *  @property load $load 
+ *  @property input $input 
+ *  @property db $db
+ *  @property session $session
+ */
+
+class Excell extends CI_Controller
+{
+
+    public function cobaexport()
+    {
+        $spreadsheet = new Spreadsheet(); // instantiate Spreadsheet
+
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // manually set table data value
+        $sheet->setCellValue('A1', 'Gipsy Danger');
+        $sheet->setCellValue('A2', 'Gipsy Avenger');
+        $sheet->setCellValue('A3', 'Striker Eureka');
+
+        $writer = new Xlsx($spreadsheet); // instantiate Xlsx
+
+        $filename = 'list-of-dummy'; // set filename for excel file to be exported
+
+        header('Content-Type: application/vnd.ms-excel'); // generate excel file
+        header('Content-Disposition: attachment;filename="' . $filename . '.xlsx"');
+        header('Cache-Control: max-age=0');
+
+        $writer->save('php://output');	// download file 
+    }
+}
